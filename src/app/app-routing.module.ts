f6f13c31cd558fc1ea08b1361ca1396a198290aa
Simon Vennat <56834754+simven@users.noreply.github.com>
2020-03-24 20:02:07 +0100

    Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {GameComponent} from './game/game.component';
 import {HomeComponent} from './home/home.component';
+import {NavComponent} from './layout/nav/nav.component';
+import {LoginComponent} from './auth/login/login.component';
+import {RegisterComponent} from './auth/register/register.component';
 
 
 const routes: Routes = [
-  { path: '',   redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent},
-  { path: 'game', component: GameComponent},
-  { path: '**', component: PageNotFoundComponent }
+  {path: '',   redirectTo: '/home', pathMatch: 'full' },
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'game', component: GameComponent},
+  {path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
