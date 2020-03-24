59419816b93d42602413c6985aa34df1f05b961f
Bluesoul02 <kevinetjunior@hotmail.fr>
2020-03-24 19:25:46 +0100

    merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import {RouterModule} from '@angular/router';
 import {NavComponent} from './layout/nav/nav.component';
 import {GameComponent} from './game/game.component';
 import {AngularMaterialModule} from './angular-material.module';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, NavComponent, GameComponent],
-  imports: [BrowserModule, RouterModule, AngularMaterialModule],
+  imports: [BrowserModule, RouterModule.forRoot([]), AngularMaterialModule, HttpClientModule],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
