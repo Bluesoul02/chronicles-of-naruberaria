72f99470d74e432e843e17351d55c0f0ac98d5f9
Bluesoul02 <kevinetjunior@hotmail.fr>
2020-03-24 18:57:58 +0100

    gameComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,4 @@
-import {Component} from '@angular/core';
-import * as Phaser from 'phaser';
-
-
-class Game extends Phaser.Scene {
-  helloWorld: Phaser.GameObjects.Text;
-
-  init() {
-    this.cameras.main.setBackgroundColor('#24252A');
-  }
-
-  create() {
-    this.helloWorld = this.add.text(
-      this.cameras.main.centerX,
-      this.cameras.main.centerY,
-      'Hello World', {
-        font: '40px Arial',
-        fill: '#ffffff'
-      }
-    );
-    this.helloWorld.setOrigin(0.5);
-
-  }
-
-  update() {
-    this.helloWorld.angle += 1;
-  }
-
-  setAngle(angle: number) {
-    this.helloWorld.angle = angle;
-  }
-}
-
-interface GameInstance extends Phaser.Types.Core.GameConfig {
-  instance: Phaser.Game;
-}
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -41,29 +6,6 @@ interface GameInstance extends Phaser.Types.Core.GameConfig {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  initialize = false;
-  game: GameInstance = {
-    width: '100%',
-    height: '100%',
-    type: Phaser.AUTO,
-    scene: Game,
-    instance: null
-  };
-
-  getInstance() {
-    return this.game.instance;
-  }
-
-  initializeGame() {
-    this.initialize = true;
-  }
-
-  changeAngle() {
-    const instance = this.getInstance();
-    instance.scene.scenes.forEach(scene => {
-      if (scene.sys.isActive() && scene instanceof Game) {
-        scene.setAngle(0);
-      }
-    });
-  }
+  title = 'taches-passport-front';
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule} from '@angular/router';
+import {NavComponent} from './layout/nav/nav.component';
+import {GameComponent} from './game/game.component';
+import {AngularMaterialModule} from './angular-material.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, NavComponent, GameComponent],
+  imports: [BrowserModule, RouterModule, AngularMaterialModule],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
