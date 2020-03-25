7976c4aa6128aca404cbbd27d94c8a83a3261e95
Bluesoul02 <kevinetjunior@hotmail.fr>
2020-03-25 10:11:15 +0100

    bouton jeu TODO régler: le bug affichage si possible

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
+import {GameComponent} from '../game/game.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'game', component: GameComponent},
 ];
 
 @NgModule({
