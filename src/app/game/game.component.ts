2d51a0acc96ff34458255fd564d8f254dc2956d7
Boisedu Maxence <maxence_boisedu@ens.univ-artois.fr>
2020-03-25 14:00:24 +0100

    essai phaser

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -3,34 +3,50 @@ import * as Phaser from 'phaser';
 
 
 class Game extends Phaser.Scene {
-  helloWorld: Phaser.GameObjects.Text;
+  platforms;
+  player;
 
   init() {
     this.cameras.main.setBackgroundColor('#24252A');
   }
 
   preload() {
+    this.load.spritesheet('dude','../assets/dude.png',{framWidth:32, frameHeight:48});
   }
 
   create() {
-    this.helloWorld = this.add.text(
-      this.cameras.main.centerX,
-      this.cameras.main.centerY,
-      'ALED', {
-        font: '40px Arial',
-        fill: '#ffffff'
-      }
-    );
-    this.helloWorld.setOrigin(0.5);
+
+    this.player = this.physics.add.sprite(100, 450, 'dude');
+
+    this.player.setBounce(0.2);
+    this.player.setCollideWorldBounds(true);
+
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'turn',
+      frames: [ { key: 'dude', frame: 4 } ],
+      frameRate: 20
+    });
+
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1
+    });
 
   }
 
   update() {
-    this.helloWorld.angle += 1;
   }
 
   setAngle(angle: number) {
-    this.helloWorld.angle = angle;
   }
 }
 
