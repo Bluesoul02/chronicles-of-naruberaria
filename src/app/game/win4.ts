2e4c99217b56fdff642c5cc497954ce71a936ab0
Bluesoul02 <kevinetjunior@hotmail.fr>
2020-03-31 14:24:04 +0200

    ff7 win Music for last winscreen

--- src/app/game/win4.ts
+++ src/app/game/win4.ts
@@ -1,5 +1,5 @@
-import {GameCreator} from './GameCreator';
-import {Niveau1} from './niveau1';
+import { GameCreator } from './GameCreator';
+import { Niveau1 } from './niveau1';
 
 export class Win4 extends Phaser.Scene {
 
@@ -7,16 +7,19 @@ export class Win4 extends Phaser.Scene {
     super(config);
   }
 
-  init() {}
+  init() { }
 
   preload() {
+    this.load.audio('winMusic', 'assets/lvl4/ff7win.mp3');
     this.load.image('win4', 'assets/lvl4/win.png');
     console.log('menu');
   }
 
-  create()  {
+  create() {
+    this.sound.play('winMusic');
+    this.sound.volume = 0.2;
     GameCreator.createWinToMenu(this, 'win4');
   }
 
-  update(time, delta) {}
+  update(time, delta) { }
 }
